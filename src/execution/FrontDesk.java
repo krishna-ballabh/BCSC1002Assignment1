4008f96ab28bb0b1d30285653adbcd391aeac759
Krishna ballabh <krishna.ballabh_cs19@gla.ac.in>
2020-08-29 12:55:50 +0530

    print the statement in execution class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,6 +8,11 @@ package execution;
 
 public class FrontDesk {
     public static void main(String[] args) {
-
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n" +
+                "How may I help you today?\n" +
+                "1. Issue a new book for me.\n" +
+                "2. Return a previously issues book for me.\n" +
+                "3. Show me all my issues books.\n" +
+                "4. Exit.\n");
     }
 }
