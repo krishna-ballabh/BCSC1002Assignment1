2b86609b9e4f9bec35f4a4084dc2c4506d89b511
Krishna ballabh <krishna.ballabh_cs19@gla.ac.in>
2020-08-29 13:35:09 +0530

    add do while loop to execution class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -13,34 +13,32 @@ import java.util.Scanner;
 
 public class FrontDesk {
     public static void main(String[] args) {
-        Student student;
+        Student student = new Student();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n" +
-                "How may I help you today?\n" +
-                "1. Issue a new book for me.\n" +
-                "2. Return a previously issues book for me.\n" +
-                "3. Show me all my issues books.\n" +
-                "4. Exit.\n");
-        System.out.println("Enter your Name");
-        String nameOfStudent = scanner.nextLine();
-        System.out.println("Enter your University Roll Number");
-        long universityRollNumber = scanner.nextLong();
-        System.out.println("Enter your choice");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        switch (choice) {
-            case 1:
-                System.out.println("Enter Book Name : ");
-                String bookName = scanner.nextLine();
-                System.out.println("Enter Author Name : ");
-                scanner.nextLine();
-                String authorName = scanner.nextLine();
-                System.out.println("Enter ISBN Number of Book");
-                scanner.nextLine();
-                String  isbnNumber = scanner.nextLine();
-                Book book = new Book(bookName, authorName, isbnNumber);
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n" +
+                    "How may I help you today?\n" +
+                    "1. Issue a new book for me.\n" +
+                    "2. Return a previously issues book for me.\n" +
+                    "3. Show me all my issues books.\n" +
+                    "4. Exit.\n");
+            System.out.println("Enter your choice");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Book Name : ");
+                    String bookName = scanner.nextLine();
+                    System.out.println("Enter Author Name : ");
+                    scanner.nextLine();
+                    String authorName = scanner.nextLine();
+                    System.out.println("Enter ISBN Number of Book");
+                    scanner.nextLine();
+                    String isbnNumber = scanner.nextLine();
+                    Book book = new Book(bookName, authorName, isbnNumber);
 
-        }
+            }
+        }while (true);
 
     }
 
